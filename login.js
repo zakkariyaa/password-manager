0e8c1bae3b53bf281f0eb3a0f095313dc1eaa6be
Zakarie Yaris <info@zakariya.me>
2022-11-24 18:51:22 +0000

    Update login.js

--- login.js
+++ login.js
@@ -1,21 +1,19 @@
 const user = JSON.parse(localStorage.getItem('user'));
 
-// if (user) {
-//   if (user.logged) {
-//     if (window.location.pathname === './index.html') {
-//       null;
-//     } else {
-//       window.location.assign('./index.html');
-//     }
-//   } else {
-//     if (window.location.pathname === './login.html') {
-//       null;
-//     } else {
-//       window.location.assign('./login.html');
-//     }
-//   }
-// } else {
-//   null;
-// }
-console.log('cool')
-console.log(user)
+if (user) {
+  if (user.logged) {
+    if (window.location.pathname === './index.html') {
+      null;
+    } else {
+      window.location.assign('./index.html');
+    }
+  } else {
+    if (window.location.pathname === './login.html') {
+      null;
+    } else {
+      window.location.assign('./login.html');
+    }
+  }
+} else {
+  null;
+}
